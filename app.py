0e39ae83350483da835fd0669ad504dc717eded1
venkatsubash2003 <venkatsaisubash.2003@gmail.com>
2024-09-12 16:15:51 +0530

    app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def predict_datapoint():
         return render_template('home.html',results=results[0])
     
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8080)        
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8080)
 
 
